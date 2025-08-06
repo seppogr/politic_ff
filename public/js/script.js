6e52380726669ab3ebe1580082d8e62546b91d13
seppogr <seppo.grubert@tuni.fi>
2025-08-06 09:39:15 +0300

    Public folder imported

---
+++ public/js/script.js
@@ -0,0 +1,136 @@
+
+
+function sortTable(columNNumber) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[columNNumber];
+      y = rows[i + 1].getElementsByTagName("TD")[columNNumber];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+};
+
+function sortTableNumber(columNNumber) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[columNNumber];
+      y = rows[i + 1].getElementsByTagName("TD")[columNNumber];
+      //check if the two rows should switch place:
+      if ((parseInt(x.innerHTML) < parseInt(y.innerHTML))) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+};
+
+function sortTable1(tableClass, n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+  table = document.getElementsByClassName(tableClass)[0];
+  switching = true;
+  dir = "asc";
+  while (switching) {
+      switching = false;
+      rows = table.getElementsByTagName("TR");
+      for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          var xContent = (isNaN(x.innerHTML))
+              ? (x.innerHTML.toLowerCase() === '-')
+                    ? 0 : x.innerHTML.toLowerCase()
+              : parseFloat(x.innerHTML);
+          var yContent = (isNaN(y.innerHTML))
+              ? (y.innerHTML.toLowerCase() === '-')
+                    ? 0 : y.innerHTML.toLowerCase()
+              : parseFloat(y.innerHTML);
+          if (dir == "asc") {
+              if (xContent > yContent) {
+                  shouldSwitch= true;
+                  break;
+              }
+          } else if (dir == "desc") {
+              if (xContent < yContent) {
+                  shouldSwitch= true;
+                  break;
+              }
+          }
+      }
+      if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount ++;
+      } else {
+          if (switchcount == 0 && dir == "asc") {
+              dir = "desc";
+              switching = true;
+          }
+      }
+   }
+}
+
+function hideContent(hideThis) {
+  document.getElementById(hideThis).style.display = 'none'
+};
+
+
+
+
+
+function getForm(e) {
+    var form = document.getElementById("task").value;
+    console.log(form);
+
+    e.preventDefault(); // Or return false.
+};
+
+module.exports = {
+    hideContent: hideContent
+}
