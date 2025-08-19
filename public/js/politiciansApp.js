1de457a3a5890735643a05f85fdaf6bf2d851259
seppogr <seppo.grubert@tuni.fi>
2025-08-19 10:24:17 +0300

    some css added, minor finetuning

--- public/js/politiciansApp.js
+++ public/js/politiciansApp.js
@@ -8,19 +8,22 @@ $(function() {
             if (!data || !data.politicians) return;
             data.politicians.forEach((politician) => {
                 $(".modal-body").append(
-                    `<div>
-                    <div class="politician-name">
-                     ${politician.name.first} ${politician.name.last}
-                    </div>
-                    <div class="politician-picked" data-id="${politician._id}>
-                      ${politician.joined}
-                    </div>
-                    <div class="course-description">
-
-                    </div>
-                    <div>
-                    <button class="pick-button" data-id="${politician._id}">Valitse tiimiisi</button>
-                    </div>
+                    `
+                    <div class="gridOfPoliticians">
+                        <div class="politicians">
+                            <div class="politician-picked" data-id="${politician._id}>
+                                ${politician.joined}
+                            </div>
+                            <div class="photo-politician">
+                                 <a href="/politicians/${politician._id}"> <img class="photo" src="/images/mps/${politician._id}.jpg" height="75" width="51"></a>
+                            </div>
+                            <div class="politician-name">
+                                ${politician.name.first} ${politician.name.last}
+                            </div>
+                            <div>
+                            <button class="pick-button" data-id="${politician._id}">Valitse tiimiisi</button>
+                            </div>
+                        </div>
                     </div>`
                 );
             });
@@ -32,14 +35,15 @@ $(function() {
         });
     });
 
-//
-   //<button class='${course.joined ? "joined-button" : "join-button"}' data-id="${course._id}>${course.joined ? "Joined" : "Join"}
-     //                 </button>
+// When .pick-button is clicked it gains class .clicked. When there is a button with a .clicked class
+// it is assigned to $button variable. This button variable can then be modified and user notified
+// that the politician has been selected and added to user's team
+
 let addPickButtonListener = () => {
-             $(document).on("click", ".pick-button", function() {
-                $(this).addClass("clicked");
-                var politicianId = $(this).attr("data-id");
-                let $button = $(".clicked");
+    $(document).on("click", ".pick-button", function() {
+        $(this).addClass("clicked");
+        var politicianId = $(this).attr("data-id");
+        let $button = $(".clicked");
         $.get(`api/politicians/${politicianId}/pick`, (results = {}) => {
             let data = results.data;
             if (data && data.success) {
@@ -56,97 +60,45 @@ let addPickButtonListener = () => {
 };
 
 // this prints out the associated courses for the user that has logged in, as a table
-//let printPoliticianData = () => {
+// waits for a click on #politicians-button, then empties the .modal-body div
+// and finally fetches the current logged in users politicians from the database
+
     $(function() {
     let data;
-       $( "#politicians-button" ).on( "click", function() {
+    $( "#politicians-button" ).on( "click", function() {
         $( ".modal-body" ).text("");
-         $.get("api/politicians/userpoliticians", (results = {}) => {
-             data = results.data;
-
-            $( ".modal-body" ).append(`
-            <div>
-            Sinun tiimisi
-            </div>
-
-
-            <thead>
-                <tr>
-                    <th>Nimi</th>
-                    <th>Kuva</th>
-                    <th>Puolue</th>
-                    <th>Logo</th>
-                </tr>
-            </thead>
-
-
-            `);
-             data.politicians.forEach((politician) => {
+        $.get("api/politicians/userpoliticians", (results = {}) => {
+            data = results.data;
+            data.politicians.forEach((politician) => {
                 $(".modal-body").append(
-
-                    `
-
-            <div class="center">
-            <ul>
-            <li>
-                <h2>
-
-                ${politician.name.first} ${politician.name.last}
-                </h2>
-            </li>
-            <li>
-               <a href="/politicians/${politician._id}"> <img class="photo" src="/images/mps/${politician._id}.jpg" height="75" width="51"></a>
-               </li>
-            <li>
-            <a href="/politicians/${politician.party}/showParty"><img class="photo" src="/images/logos/${politician.party}.png" height="51" width="51"></a>
-            </li>
-            <li>
-                <a href="/politicians/${politician.party}/showParty">${politician.party} </a>
-            </li>
-            <li>
-            Saadut pisteet: ${politician.points.won}
-            </li>
-                </ul>
-            <div>
-
+                `
+                <div class="center">
+                    <ul>
+                        <li>
+                            <h2>
+                            ${politician.name.first} ${politician.name.last}
+                            </h2>
+                        </li>
+                        <li>
+                            <a href="/politicians/${politician._id}"> <img class="photo" src="/images/mps/${politician._id}.jpg" height="75" width="51"></a>
+                        </li>
+                        <li>
+                            <a href="/politicians/${politician.party}/showParty"><img class="photo" src="/images/logos/${politician.party}.png" height="51" width="51"></a>
+                        </li>
+                        <li>
+                            <a href="/politicians/${politician.party}/showParty">${politician.party} </a>
+                        </li>
+                        <li>
+                            Saadut pisteet: ${politician.points.won}
+                        </li>
+                    </ul>
+                <div>
                     `
                 );
             });
             });
-
-
-
-
-        //     for (let i = 0; i < data.courses.length; i++) {
-        //     //if(data.courses[i].joined == true){
-        //          $( ".modal-footer" ).append(
-        // `
-        // <p>
-        // Title:
-        //  ${JSON.stringify(data.courses[i].title)}
-        // </p>
-
-        // <p>
-        // Description:
-        //  ${JSON.stringify(data.courses[i].description)}
-        // </p>
-
-        // <p>
-        // Cost:  ${JSON.stringify(data.courses[i].cost)}
-        // </p>
-
-        // `
-        //         );
-        //     }
-
-         });
-
-
-       // }
-
-
-
-     });
+        });
+    });
 
 
 // $(function() {
